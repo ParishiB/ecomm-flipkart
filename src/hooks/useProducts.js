339b63d5d320f3f16889a74d2a3db9ddd989b6dd
PARISHI BHANGE <parishibhange@PARISHIs-MacBook-Pro.local>
2024-08-18 22:59:35 +0530

    check

--- src/hooks/useProducts.js
+++ src/hooks/useProducts.js
@@ -1,37 +1,3 @@
-// import { useState, useEffect } from "react";
-// import { getProductById, getProducts } from "../Util/httpClient";
-
-// const useProducts = (id = null) => {
-//   const [products, setProducts] = useState([]);
-//   const [product, setProduct] = useState(null);
-//   const [loading, setLoading] = useState(true);
-//   const [error, setError] = useState(null);
-
-//   useEffect(() => {
-//     const fetchProducts = async () => {
-//       try {
-//         if (id) {
-//           const response = await getProductById(id);
-//           setProduct(response.data);
-//         } else {
-//           const response = await getProducts();
-//           setProducts(response.data);
-//         }
-//       } catch (error) {
-//         console.error("Error fetching products:", error);
-//         setError(error);
-//       } finally {
-//         setLoading(false);
-//       }
-//     };
-
-//     fetchProducts();
-//   }, [id]);
-
-//   return { products, product, loading, error };
-// };
-
-// export default useProducts;
 import { useState, useEffect } from "react";
 import { getProductById, getProducts } from "../Util/httpClient";
 
