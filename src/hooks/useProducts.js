971cbfe62b66fe1491ecda5492c192b64c0c4098
PARISHI BHANGE <parishibhange@PARISHIs-MacBook-Pro.local>
2024-08-18 22:52:51 +0530

    check

--- src/hooks/useProducts.js
+++ src/hooks/useProducts.js
@@ -1,3 +1,37 @@
+// import { useState, useEffect } from "react";
+// import { getProductById, getProducts } from "../Util/httpClient";
+
+// const useProducts = (id = null) => {
+//   const [products, setProducts] = useState([]);
+//   const [product, setProduct] = useState(null);
+//   const [loading, setLoading] = useState(true);
+//   const [error, setError] = useState(null);
+
+//   useEffect(() => {
+//     const fetchProducts = async () => {
+//       try {
+//         if (id) {
+//           const response = await getProductById(id);
+//           setProduct(response.data);
+//         } else {
+//           const response = await getProducts();
+//           setProducts(response.data);
+//         }
+//       } catch (error) {
+//         console.error("Error fetching products:", error);
+//         setError(error);
+//       } finally {
+//         setLoading(false);
+//       }
+//     };
+
+//     fetchProducts();
+//   }, [id]);
+
+//   return { products, product, loading, error };
+// };
+
+// export default useProducts;
 import { useState, useEffect } from "react";
 import { getProductById, getProducts } from "../Util/httpClient";
 
@@ -9,6 +43,9 @@ const useProducts = (id = null) => {
 
   useEffect(() => {
     const fetchProducts = async () => {
+      setLoading(true);
+      setError(null);
+
       try {
         if (id) {
           const response = await getProductById(id);
--- src/pages/product/Product.jsx
+++ src/pages/product/Product.jsx
@@ -1,8 +1,181 @@
+// import { useParams } from "react-router-dom";
+// import { useCallback, useState, useEffect } from "react";
+// import { useDispatch, useSelector } from "react-redux";
+// import useProducts from "../../hooks/useProducts";
+// import useCart from "../../hooks/useCart";
+// import { ClipLoader } from "react-spinners";
+// import rating from "../../assets/rating.png";
+// import { FaTags } from "react-icons/fa";
+// import {
+//   increaseQuantity,
+//   decreaseQuantity,
+//   addToCart,
+//   removeFromCart,
+//   deleteParticularItemsCart,
+// } from "../../state/product/productSlice";
+// import { useNavigate } from "react-router-dom";
+// import useScreenInfo from "../../hooks/useScreenInfo";
+// const Product = () => {
+//   const { id } = useParams();
+//   const navigate = useNavigate();
+//   const dispatch = useDispatch();
+//   const { product, loading, error } = useProducts(id);
+//   const cart = useSelector((state) => state.product.cart);
+//   const { isMobileView, isLargeMobile, isLargeView, isIpad } = useScreenInfo();
+//   const goToCart = () => {
+//     navigate("/cart");
+//   };
+//   const handleAddClick = useCallback(
+//     () => dispatch(addToCart(product)),
+//     [dispatch, product]
+//   );
+//   const handleRemoveClick = useCallback(
+//     () => dispatch(deleteParticularItemsCart(product)),
+//     [dispatch, product]
+//   );
+//   const handleIncreaseClick = useCallback(
+//     () => dispatch(increaseQuantity(product)),
+//     [dispatch, product]
+//   );
+//   const handleDecreaseClick = useCallback(
+//     () => dispatch(decreaseQuantity(product)),
+//     [dispatch, product]
+//   );
+//   const offers = [
+//     {
+//       text: "Get ₹50 Instant Discount on first Flipkart UPI transaction on order of ₹200 and above",
+//       condition: "Bank Offer",
+//       terms: "T&C",
+//     },
+//   ];
+//   if (loading) {
+//     return (
+//       <div className="flex items-center justify-center h-screen">
+//         <ClipLoader color="#2974f0" size={50} />
+//       </div>
+//     );
+//   }
+
+//   if (error) {
+//     return (
+//       <div className="text-black p-20">Error fetching product details.</div>
+//     );
+//   }
+
+//   if (!product) {
+//     return <div className="text-black p-20">Product not found.</div>;
+//   }
+
+//   const cartItem = cart.find((item) => item.id === product.id);
+//   const isInCart = !!cartItem;
+
+//   return (
+//     <div
+//       className={`p-20 pt-10 ${
+//         isMobileView || isLargeMobile
+//           ? "flex flex-col p-0"
+//           : "grid grid-cols-[30%_auto]"
+//       }`}
+//     >
+//       <div className="text-white mb-10">
+//         <img
+//           src={product.image}
+//           alt={product.title}
+//           className={`p-20 pt-10 w-full mb-4${
+//             isMobileView || isLargeMobile
+//               ? "h-[200px] flex justify-center"
+//               : " "
+//           }`}
+//         />
+//         <div className="flex gap-1 px-5">
+//           <button
+//             onClick={handleRemoveClick}
+//             disabled={!isInCart}
+//             className="border w-full py-2 bg-[#ff9f00]"
+//           >
+//             Remove from Cart
+//           </button>
+//           <button
+//             onClick={handleAddClick}
+//             disabled={isInCart}
+//             className="border bg-[#fb641b] py-2  w-full"
+//           >
+//             Add to Cart
+//           </button>
+//         </div>
+//         <div className="flex justify-center m-5">
+//           {isInCart && (
+//             <div className="flex text-black">
+//               <button
+//                 className="border rounded-full px-6 py-2"
+//                 onClick={handleDecreaseClick}
+//               >
+//                 -
+//               </button>
+//               <p className="text-gray-600 p-5">{cartItem.quantity}</p>
+//               <button
+//                 className="border rounded-full px-6 py-2"
+//                 onClick={handleIncreaseClick}
+//               >
+//                 +
+//               </button>
+//             </div>
+//           )}
+//         </div>
+//       </div>
+//       <div
+//         className={`m-10 ${
+//           isMobileView || isLargeMobile ? "mt-0  items-center" : ""
+//         }`}
+//       >
+//         <p className="text-gray-800 mb-4 font-extrabold">{product.title}</p>
+//         <p className="text-gray-500 font-bold flex items-center">
+//           <img src={rating} className="h-[20px] mr-2" alt="rating" />
+//           31,903 Ratings & 2,000 Review
+//         </p>
+//         <p className="text-lg font-semibold my-8">₹{product.price}</p>
+//         <p className="text-gray-800 mb-4">{product.description}</p>
+//         <div className="space-y-4">
+//           <div
+//             className={`flex items-center gap-2 ${
+//               isMobileView || isLargeMobile ? "w-full" : "p-2"
+//             }`}
+//           >
+//             <FaTags
+//               className={`text-green-600 ${
+//                 isMobileView || isLargeMobile ? "hidden" : ""
+//               }`}
+//             />
+//             <span className="font-semibold inline">
+//               Get ₹50 Instant Discount on first Flipkart UPI transaction on
+//               order of ₹200 and above
+//             </span>
+//             <span className="text-blue-500 font-bold">T&C</span>
+//           </div>
+//         </div>
+//         <p
+//           className={`flex  text-blue-500 font-bold ${
+//             isMobileView || isLargeMobile ? "py-5" : ""
+//           }`}
+//         >
+//           View 40 MORE OFFERS
+//         </p>
+//         <button
+//           onClick={goToCart}
+//           className="bg-[#fb641b] text-white p-2 font-bold my-2 mt-10 px-5 "
+//         >
+//           Go to the Cart
+//         </button>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default Product;
 import { useParams } from "react-router-dom";
-import { useCallback, useState, useEffect } from "react";
+import { useCallback } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import useProducts from "../../hooks/useProducts";
-import useCart from "../../hooks/useCart";
 import { ClipLoader } from "react-spinners";
 import rating from "../../assets/rating.png";
 import { FaTags } from "react-icons/fa";
@@ -10,21 +183,23 @@ import {
   increaseQuantity,
   decreaseQuantity,
   addToCart,
-  removeFromCart,
   deleteParticularItemsCart,
 } from "../../state/product/productSlice";
 import { useNavigate } from "react-router-dom";
 import useScreenInfo from "../../hooks/useScreenInfo";
+
 const Product = () => {
   const { id } = useParams();
   const navigate = useNavigate();
   const dispatch = useDispatch();
   const { product, loading, error } = useProducts(id);
   const cart = useSelector((state) => state.product.cart);
-  const { isMobileView, isLargeMobile, isLargeView, isIpad } = useScreenInfo();
+  const { isMobileView, isLargeMobile } = useScreenInfo();
+
   const goToCart = () => {
     navigate("/cart");
   };
+
   const handleAddClick = useCallback(
     () => dispatch(addToCart(product)),
     [dispatch, product]
@@ -41,13 +216,7 @@ const Product = () => {
     () => dispatch(decreaseQuantity(product)),
     [dispatch, product]
   );
-  const offers = [
-    {
-      text: "Get ₹50 Instant Discount on first Flipkart UPI transaction on order of ₹200 and above",
-      condition: "Bank Offer",
-      terms: "T&C",
-    },
-  ];
+
   if (loading) {
     return (
       <div className="flex items-center justify-center h-screen">
@@ -82,9 +251,7 @@ const Product = () => {
           src={product.image}
           alt={product.title}
           className={`p-20 pt-10 w-full mb-4${
-            isMobileView || isLargeMobile
-              ? "h-[200px] flex justify-center"
-              : " "
+            isMobileView || isLargeMobile ? "h-[200px] flex justify-center" : ""
           }`}
         />
         <div className="flex gap-1 px-5">
@@ -98,13 +265,13 @@ const Product = () => {
           <button
             onClick={handleAddClick}
             disabled={isInCart}
-            className="border bg-[#fb641b] py-2  w-full"
+            className="border bg-[#fb641b] py-2 w-full"
           >
             Add to Cart
           </button>
         </div>
-        <div className="flex justify-center m-5">
-          {isInCart && (
+        {isInCart && (
+          <div className="flex justify-center m-5">
             <div className="flex text-black">
               <button
                 className="border rounded-full px-6 py-2"
@@ -120,12 +287,12 @@ const Product = () => {
                 +
               </button>
             </div>
-          )}
-        </div>
+          </div>
+        )}
       </div>
       <div
         className={`m-10 ${
-          isMobileView || isLargeMobile ? "mt-0  items-center" : ""
+          isMobileView || isLargeMobile ? "mt-0 items-center" : ""
         }`}
       >
         <p className="text-gray-800 mb-4 font-extrabold">{product.title}</p>
@@ -154,7 +321,7 @@ const Product = () => {
           </div>
         </div>
         <p
-          className={`flex  text-blue-500 font-bold ${
+          className={`flex text-blue-500 font-bold ${
             isMobileView || isLargeMobile ? "py-5" : ""
           }`}
         >
@@ -162,7 +329,7 @@ const Product = () => {
         </p>
         <button
           onClick={goToCart}
-          className="bg-[#fb641b] text-white p-2 font-bold my-2 mt-10 px-5 "
+          className="bg-[#fb641b] text-white p-2 font-bold my-2 mt-10 px-5"
         >
           Go to the Cart
         </button>
